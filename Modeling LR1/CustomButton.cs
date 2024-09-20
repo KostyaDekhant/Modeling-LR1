fcb4f9a88de4f3f9dd34277a00e5832ac86f3df4
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2024-09-20 16:41:40 +0700

    Добавьте файлы проекта.

---
+++ Modeling LR1/CustomButton.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Manager.RJControls
+{
+    public class CustomButton : Control
+    {
+        private Color _buttonColor = Color.FromArgb(255, 33, 40, 48);
+        private Color _hoverColor = Color.FromArgb(255, 33, 40, 48);
+        private Color _clickColor = Color.FromArgb(255, 33, 40, 48);
+        private Color _textColor = Color.FromArgb(255, 233, 238, 245);
+        private Color _borderColor = Color.FromArgb(255, 147, 154, 163);
+        private bool _isHovering = false;
+        private bool _isClicking = false;
+        private int _cornerRadius = 0;
+        private int _borderThickness = 0;
+        private bool _enableAnimation = true;
+
+        public Color ButtonColor
+        {
+            get => _buttonColor;
+            set
+            {
+                _buttonColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color HoverColor
+        {
+            get => _hoverColor;
+            set
+            {
+                _hoverColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color ClickColor
+        {
+            get => _clickColor;
+            set
+            {
+                _clickColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color TextColor
+        {
+            get => _textColor;
+            set
+            {
+                _textColor = value;
+                Invalidate();
+            }
+        }
+
+        public Color BorderColor
+        {
+            get => _borderColor;
+            set
+            {
+                _borderColor = value;
+                Invalidate();
+            }
+        }
+
+        public int CornerRadius
+        {
+            get => _cornerRadius;
+            set
+            {
+                _cornerRadius = Math.Max(0, value);
+                Invalidate();
+            }
+        }
+
+        public int BorderThickness
+        {
+            get => _borderThickness;
+            set
+            {
+                _borderThickness = Math.Max(0, value);
+                Invalidate();
+            }
+        }
+
+        public bool EnableAnimation
+        {
+            get => _enableAnimation;
+            set => _enableAnimation = value;
+        }
+
+        public CustomButton()
+        {
+            this.DoubleBuffered = true;
+            this.ResizeRedraw = true;
+            this.Size = new Size(100, 40);
+            this.Font = new Font("Arial", 8, FontStyle.Bold);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            DrawButton(e.Graphics);
+        }
+
+        /*private void DrawButton(Graphics g)
+        {
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            Color currentColor = _buttonColor;
+
+            if (_isClicking)
+                currentColor = _clickColor;
+            else if (_isHovering)
+                currentColor = _hoverColor;
+
+            Rectangle rect = ClientRectangle;
+            rect.Width -= 1;
+            rect.Height -= 1;
+
+            using (Brush brush = new SolidBrush(currentColor))
+            using (Pen pen = new Pen(_borderColor, _borderThickness))
+            {
+                if (_cornerRadius > 0)
+                {
+                    GraphicsPath path = GetRoundedRectanglePath(rect, _cornerRadius);
+                    g.FillPath(brush, path);
+                    g.DrawPath(pen, path);
+                }
+                else
+                {
+                    g.FillRectangle(brush, rect);
+                    g.DrawRectangle(pen, rect);
+                }
+            }
+
+            using (Brush textBrush = new SolidBrush(_textColor))
+            {
+                SizeF textSize = g.MeasureString(Text, Font);
+                PointF textPosition = new PointF(
+                    (Width - textSize.Width) / 2,
+                    (Height - textSize.Height) / 2);
+                g.DrawString(Text, Font, textBrush, textPosition);
+            }
+        }
+        */
+        private GraphicsPath GetRoundedRectanglePath(Rectangle rect, int radius)
+        {
+            int diameter = radius * 2;
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(rect.X, rect.Y, diameter, diameter, 180, 90);
+            path.AddArc(rect.Right - diameter, rect.Y, diameter, diameter, 270, 90);
+            path.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - diameter, diameter, diameter, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            _isHovering = true;
+            if (_enableAnimation)
+                Invalidate();
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            _isHovering = false;
+            if (_enableAnimation)
+                Invalidate();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            _isClicking = true;
+            if (_enableAnimation)
+                Invalidate();
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            _isClicking = false;
+            if (_enableAnimation)
+                Invalidate();
+        }
+
+        private bool _borderVisible = true;
+
+        public bool BorderVisible
+        {
+            get => _borderVisible;
+            set
+            {
+                _borderVisible = value;
+                Invalidate();
+            }
+        }
+
+        // ... (other methods remain the same)
+
+        private void DrawButton(Graphics g)
+        {
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            Color currentColor = _buttonColor;
+
+            if (_isClicking)
+                currentColor = _clickColor;
+            else if (_isHovering)
+                currentColor = _hoverColor;
+
+            Rectangle rect = ClientRectangle;
+            rect.Width -= 1;
+            rect.Height -= 1;
+
+            using (Brush brush = new SolidBrush(currentColor))
+            {
+                if (_cornerRadius > 0)
+                {
+                    GraphicsPath path = GetRoundedRectanglePath(rect, _cornerRadius);
+                    g.FillPath(brush, path);
+                    if (_borderVisible)
+                    {
+                        using (Pen pen = new Pen(_borderColor, _borderThickness))
+                        {
+                            g.DrawPath(pen, path);
+                        }
+                    }
+                }
+                else
+                {
+                    g.FillRectangle(brush, rect);
+                    if (_borderVisible)
+                    {
+                        using (Pen pen = new Pen(_borderColor, _borderThickness))
+                        {
+                            g.DrawRectangle(pen, rect);
+                        }
+                    }
+                }
+            }
+
+            using (Brush textBrush = new SolidBrush(_textColor))
+            {
+                SizeF textSize = g.MeasureString(Text, Font);
+                PointF textPosition = new PointF(
+                    (Width - textSize.Width) / 2,
+                    (Height - textSize.Height) / 2);
+                g.DrawString(Text, Font, textBrush, textPosition);
+            }
+        }
+    
+    }
+}
---
+++ Modeling LR1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Modeling_LR1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            grid_input = new DataGridView();
+            input_lb = new Label();
+            calc_bttn = new Time_Manager.RJControls.CustomButton();
+            calc_panel = new Panel();
+            line_lb2 = new Label();
+            line_lb = new Label();
+            a_b_lb = new Label();
+            solve_lb = new Label();
+            grid_output = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)grid_input).BeginInit();
+            calc_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)grid_output).BeginInit();
+            SuspendLayout();
+            // 
+            // grid_input
+            // 
+            grid_input.BackgroundColor = Color.FromArgb(1, 4, 9);
+            grid_input.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            grid_input.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            grid_input.Location = new Point(53, 82);
+            grid_input.Name = "grid_input";
+            grid_input.RowHeadersWidth = 51;
+            grid_input.Size = new Size(175, 207);
+            grid_input.TabIndex = 0;
+            // 
+            // input_lb
+            // 
+            input_lb.AutoSize = true;
+            input_lb.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            input_lb.ForeColor = Color.White;
+            input_lb.Location = new Point(53, 43);
+            input_lb.Name = "input_lb";
+            input_lb.Size = new Size(105, 20);
+            input_lb.TabIndex = 1;
+            input_lb.Text = "Ввод данных";
+            // 
+            // calc_bttn
+            // 
+            calc_bttn.BorderColor = Color.FromArgb(147, 154, 163);
+            calc_bttn.BorderThickness = 0;
+            calc_bttn.BorderVisible = true;
+            calc_bttn.ButtonColor = Color.FromArgb(33, 40, 48);
+            calc_bttn.ClickColor = Color.FromArgb(33, 40, 48);
+            calc_bttn.CornerRadius = 0;
+            calc_bttn.EnableAnimation = true;
+            calc_bttn.Font = new Font("Arial", 7F, FontStyle.Bold);
+            calc_bttn.HoverColor = Color.FromArgb(33, 40, 48);
+            calc_bttn.Location = new Point(145, 295);
+            calc_bttn.Name = "calc_bttn";
+            calc_bttn.Size = new Size(83, 34);
+            calc_bttn.TabIndex = 2;
+            calc_bttn.Text = "Построить";
+            calc_bttn.TextColor = Color.FromArgb(233, 238, 245);
+            calc_bttn.Click += calc_bttn_Click;
+            // 
+            // calc_panel
+            // 
+            calc_panel.BorderStyle = BorderStyle.FixedSingle;
+            calc_panel.Controls.Add(line_lb2);
+            calc_panel.Controls.Add(line_lb);
+            calc_panel.Controls.Add(a_b_lb);
+            calc_panel.Location = new Point(270, 82);
+            calc_panel.Name = "calc_panel";
+            calc_panel.Size = new Size(783, 207);
+            calc_panel.TabIndex = 3;
+            // 
+            // line_lb2
+            // 
+            line_lb2.AutoSize = true;
+            line_lb2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            line_lb2.ForeColor = Color.White;
+            line_lb2.Location = new Point(45, 64);
+            line_lb2.Name = "line_lb2";
+            line_lb2.Size = new Size(14, 20);
+            line_lb2.TabIndex = 2;
+            line_lb2.Text = "|";
+            // 
+            // line_lb
+            // 
+            line_lb.AutoSize = true;
+            line_lb.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            line_lb.ForeColor = Color.White;
+            line_lb.Location = new Point(45, 44);
+            line_lb.Name = "line_lb";
+            line_lb.Size = new Size(14, 20);
+            line_lb.TabIndex = 1;
+            line_lb.Text = "|";
+            // 
+            // a_b_lb
+            // 
+            a_b_lb.AutoSize = true;
+            a_b_lb.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            a_b_lb.ForeColor = Color.White;
+            a_b_lb.Location = new Point(30, 25);
+            a_b_lb.Name = "a_b_lb";
+            a_b_lb.Size = new Size(29, 60);
+            a_b_lb.TabIndex = 0;
+            a_b_lb.Text = "A |\r\n   \r\nB ";
+            // 
+            // solve_lb
+            // 
+            solve_lb.AutoSize = true;
+            solve_lb.ForeColor = Color.White;
+            solve_lb.Location = new Point(304, 319);
+            solve_lb.MinimumSize = new Size(700, 250);
+            solve_lb.Name = "solve_lb";
+            solve_lb.Size = new Size(700, 250);
+            solve_lb.TabIndex = 4;
+            // 
+            // grid_output
+            // 
+            grid_output.BackgroundColor = Color.FromArgb(1, 4, 9);
+            grid_output.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            grid_output.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            grid_output.Location = new Point(53, 348);
+            grid_output.Name = "grid_output";
+            grid_output.RowHeadersWidth = 51;
+            grid_output.Size = new Size(175, 207);
+            grid_output.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(13, 17, 23);
+            ClientSize = new Size(1059, 567);
+            Controls.Add(grid_output);
+            Controls.Add(solve_lb);
+            Controls.Add(calc_panel);
+            Controls.Add(calc_bttn);
+            Controls.Add(input_lb);
+            Controls.Add(grid_input);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)grid_input).EndInit();
+            calc_panel.ResumeLayout(false);
+            calc_panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)grid_output).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView grid_input;
+        private Label input_lb;
+        private Time_Manager.RJControls.CustomButton calc_bttn;
+        private Panel calc_panel;
+        private Label line_lb2;
+        private Label line_lb;
+        private Label a_b_lb;
+        private Label solve_lb;
+        private DataGridView grid_output;
+    }
+}
